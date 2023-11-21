2704ebf823045654e801bab4bc23af0f61ca9bbe
Miraines <sasha_sabiya@list.ru>
2023-11-21 21:47:36 +0300

    все тесты проходят лаба сделана

--- lib/ArgParser.h
+++ lib/ArgParser.h
@@ -20,12 +20,18 @@ class ArgParser {
     int min_values; // Минимальное количество значений для аргумента
     std::string default_value_str; // Значение по умолчанию для строкового аргумента
     bool default_value_bool; // Для флагов
-
-
+    bool is_help;
 
     // Конструктор по умолчанию для инициализации указателей как nullptr
-    Argument() : value_str(nullptr), value_int(nullptr), value_bool(nullptr),
-                 is_set(false), is_flag(false), is_positional(false), min_values(0), default_value_bool(false) {}
+    Argument() : value_str(nullptr),
+                 value_int(nullptr),
+                 value_bool(nullptr),
+                 is_set(false),
+                 is_flag(false),
+                 is_positional(false),
+                 min_values(0),
+                 default_value_bool(false),
+                 default_value_str(""), is_help(false) {}
   };
 
   std::string program_name_;
@@ -33,32 +39,42 @@ class ArgParser {
   bool help_requested_ = false;
   std::string help_description_;
 
+  Argument* FindArgumentByShortKey(char short_key) {
+    for (auto& argument : arguments_) {
+      if (argument.short_name == short_key) {
+        return &argument;
+      }
+    }
+    return nullptr;
+  }
+
  public:
   ArgParser(std::string name);
   ArgParser(char short_name, std::string name);
   ArgParser();
   ~ArgParser();
 
-  ArgParser& AddStringArgument(const std::string& long_name, const std::string& description = "");
-  ArgParser& AddStringArgument(char short_name, const std::string& long_name, const std::string& description = "");
+  ArgParser& AddStringArgument(const std::string& long_name);
+  ArgParser& AddStringArgument(char short_name, const std::string& long_name);
+  ArgParser& AddStringArgument(char short_name, const std::string& long_name, const std::string& description);
 
-  //Добавление численного аргумента
-  ArgParser& AddIntArgument(const std::string& long_name, const std::string& description = "");
-  ArgParser& AddIntArgument(char short_name, const std::string& long_name, const std::string& description = "");
+  ArgParser& AddIntArgument(const std::string& long_name);
+  ArgParser& AddIntArgument(char short_name, const std::string& long_name);
+  ArgParser& AddIntArgument(const std::string& long_name, const std::string& help_description_);
 
-  //Добавление флагов
-  ArgParser& AddFlag(char short_name, const std::string& long_name, const std::string& description = "");
+  ArgParser& AddFlag(char short_name, const std::string& long_name);
+  ArgParser& AddFlag(char short_name_second, const std::string& long_name, const std::string& description);
+  ArgParser& AddFlag(const std::string long_name_third, const std::string& long_name_);
 
   //Парсинг аргументов командной строки
   bool Parse(const std::vector<std::string>& args);
 
   // Установка значения по умолчанию для аргумента
+  ArgParser& DefaultString(const std::string& default_value);
   ArgParser& Default(bool default_flag);
-  ArgParser& Default(const std::string& default_value);
-
 
   // Указание, что аргумент может принимать несколько значений
-  ArgParser& MultiValue(int min_values = 1);
+  ArgParser& MultiValue(size_t min_values = 1);
 
   // Установка аргумента как позиционного
   ArgParser& Positional();
@@ -73,10 +89,15 @@ class ArgParser {
   int GetIntValue(const std::string& arg_name, int default_value = 0) const;
   bool GetFlag(const std::string& arg_name) const;
 
-  // Методы для работы с справкой
   ArgParser& AddHelp(char short_name, const std::string& long_name, const std::string& description);
   bool Help() const;
   std::string HelpDescription() const;
 
-};
+  bool CheckArgumentsAfterParsing();
+  bool ProcessPositionalArgument(const std::string& arg, size_t& positional_arg_index);
+  bool ProcessShortArgument(const std::string& arg);
+  bool ProcessLongArgument(const std::string& arg);
+
+
+  };
 }
